7f191d9ebd569cf7c571048ed1af5de0855c5830
Anastasiia Makarenko <irishmoth@yandex.ru>
2025-12-03 13:13:12 +0300

    Обновление библиотек

--- flow/src/main/java/pro/progr/flow/composable/CalendarWithFloatingElement.kt
+++ flow/src/main/java/pro/progr/flow/composable/CalendarWithFloatingElement.kt
@@ -6,7 +6,6 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -51,7 +50,7 @@ fun CalendarWithFloatingElement(content: @Composable (date : LocalDate) -> Unit,
         }
     }
 
-    BoxWithConstraints(
+    Box(
         modifier = Modifier.fillMaxSize(),
     ) {
         VerticalDatesList(nestedScrollConnectionVert, verticalScrollState, content)
